cbe40a9c9e5370fe7b65b2bf8a2b10b88565df0e
keenwon <semanwmj@gmail.com>
2018-12-30 21:50:33 +0800

    feat: 补充注释

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,5 +1,11 @@
 "use strict"
 
+/**
+ * 支持的 type 有：
+ * 'build', 'ci', 'docs', 'feat', 'fix', 'perf',
+ * 'refactor', 'revert', 'style', 'test'
+ */
+
 module.exports = {
   extends: ["@commitlint/config-angular"]
 }
