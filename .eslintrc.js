6e6eb1b99f45542677329bb9e5080d33aec5735a
keenwon <semanwmj@gmail.com>
2018-12-31 20:26:10 +0800

    feat: add max-len

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,16 @@ module.exports = {
     es6: true,
     mocha: true,
     jest: true
+  },
+  rules: {
+    // specify the maximum length of a line in your program
+    // https://eslint.org/docs/rules/max-len
+    'max-len': ['error', 100, 2, {
+      ignoreUrls: true,
+      ignoreComments: false,
+      ignoreRegExpLiterals: true,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true,
+    }]
   }
 }
