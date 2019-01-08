d07eff4c9c551bf6ef44d6dc22c324a153f72bc2
keenwon <semanwmj@gmail.com>
2019-01-08 15:56:33 +0800

    feat: 禁用强制 jsdoc

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,10 @@ module.exports = {
     jest: true
   },
   rules: {
+    // 强制使用有效的 JSDoc 注释
+    // https://eslint.org/docs/rules/valid-jsdoc
+    'valid-jsdoc': 2,
+
     // specify the maximum length of a line in your program
     // https://eslint.org/docs/rules/max-len
     'max-len': ['error', 100, 2, {
